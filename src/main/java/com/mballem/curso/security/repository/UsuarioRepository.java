b5343f5900296bf9811c0506f4487043ffbc7243
Filipe Castro <filipe.couto.10@gmail.com>
2019-09-11 17:45:46 -0300

    query para buscar um Usuario por email

---
+++ src/main/java/com/mballem/curso/security/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.mballem.curso.security.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.mballem.curso.security.domain.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	@Query("SELECT u From Usuario u WHERE u.email LIKE :email")
+	Usuario findByEmail(@Param("email") String email);
+}
